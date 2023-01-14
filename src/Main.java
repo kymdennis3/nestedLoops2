b98cb6e5942edfce26a345fe86860419cd574ef2
Kimtai <kymdennis3@gmail.com>
2023-01-14 12:52:44 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //nested loops - A loop inside a loop
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol= "";
+
+        System.out.println("Enter number of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.println("Enter number of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.println("Enter the symbol to use: ");
+        symbol = scanner.next();
+
+        for(int i = 1; i<=rows; i++){
+            System.out.println();
+            for(int j=1; j<=columns; j++){
+            System.out.print(symbol);
+        }
     }
-}
+}}
